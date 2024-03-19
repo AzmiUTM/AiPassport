1988b75abbebbe2a67288496d6062ed14832c5dd
NOORRIZAL <noorrizal@utm.my>
2024-03-19 15:38:45 +0800

    upload footers file

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,8 @@
+<div class="row align-items-center justify-content-lg-between">
+    <div class="col-xl-6">
+        <div class="copyright text-center text-lg-left text-muted">
+            &copy; {{ now()->year }} <a href="https://cict.utm.my/" class="font-weight-bold ml-1" target="_blank">Jabatan
+                Perkhidmatan Digital (UTMDigital)</a>
+        </div>
+    </div>
+</div>
